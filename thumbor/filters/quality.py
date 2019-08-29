cb36f99925255a6c2ac5182b81e8ca34ef4799f5
Jason L Connor <jason@dronedeploy.com>
2019-08-29 11:17:34 -0600

    Reverted quality to old filter

--- thumbor/filters/quality.py
+++ thumbor/filters/quality.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-'''Filter Module'''
 
 # thumbor imaging service
 # https://github.com/thumbor/thumbor/wiki
@@ -8,17 +7,12 @@
 # Licensed under the MIT license:
 # http://www.opensource.org/licenses/mit-license
 # Copyright (c) 2011 globo.com thumbor@googlegroups.com
-from tornado import gen
 
-from thumbor import Engine
 from thumbor.filters import BaseFilter, filter_method
 
 
 class Filter(BaseFilter):
 
-    @gen.coroutine
     @filter_method(BaseFilter.PositiveNumber)
-    def quality(self, details, value):
-        details.metadata['quality'] = value if value < 100 else 100
-        mode, data = yield Engine.get_image_data_as_rgb(self, details)
-        yield Engine.set_image_data(self, details, data)
+    def quality(self, value):
+        self.context.request.quality = value
